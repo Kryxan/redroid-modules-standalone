9046546092aa028ffd48af6205f3165fb08ba646
Kryxan <83790031+Kryxan@users.noreply.github.com>
2026-04-10 18:22:22 -1000

    fix: restore binder param handler

--- binder/binder.c
+++ binder/binder.c
@@ -140,6 +140,9 @@ static int binder_stop_on_user_error;
 
 static int binder_set_stop_on_user_error(const char *val,
 									 compat_kernel_param_t *kp)
+{
+	int ret;
+
 	ret = param_set_int(val, kp);
 	if (binder_stop_on_user_error < 2)
 		wake_up(&binder_user_error_wait);
